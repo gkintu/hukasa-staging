a787efffaaa912ca087b2757cc1aa7fb59b6ebd8
George Kintu <195527580+gkintu@users.noreply.github.com>
2025-09-09 14:57:20 +0200

    feat: Disable no-img-element rule for dynamic API-served images in eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@next/next/no-img-element': 'off' // Using <img> for dynamic API-served images in virtual staging app
+    }
+  }
 ];
 
 export default eslintConfig;
